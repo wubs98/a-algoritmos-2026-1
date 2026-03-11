84f1f9110f13bf90fa00ab4baca42239c9734d40
Natã <nates.bonates@gmail.com>
2026-03-11 14:56:20 -0300

    Inserindo funcionalidade para prosseguir ou encerrar programa

--- dever-02/fatorial.py
+++ dever-02/fatorial.py
@@ -7,18 +7,25 @@ def fatorial(n):
         return n * fatorial(n - 1)
 
 def medir_tempo(p):
+    operacao = "y"
     inicio = time.perf_counter()
     resultado = fatorial(p)
     fim = time.perf_counter()
     tempo = fim - inicio
     print(f"O fatorial de {num} é {resultado}")
     print(f"Tempo de Execução: {tempo:.10f} segundos")
+    operacao = str(input("Para calcular outro número, pressione qualquer tecla. Pressione N para encerrar: "))
+    if operacao == "n" or "N":
+        exit()
+    
+
+x = 0
 
 
-num = int(input("Informe um número para calcule do seu fatorial: "))
-
-
-if num < 0:
-    print("Não é possível calcular o fatorial de um número negativo.") 
-else:
-    medir_tempo(num)
+while x%2 == 0:
+    num = int(input("Informe um número para o calculo do seu fatorial: "))
+    if num < 0:
+        print("Não é possível calcular o fatorial de um número negativo.") 
+    else:
+        medir_tempo(num)
+    x = x/2
